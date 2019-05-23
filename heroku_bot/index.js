ee4ee8bebe3aa98e2f384904da377bfeac75701d
a <you@example.com>
2019-05-23 15:19:31 +0200

    tri

--- heroku_bot/index.js
+++ heroku_bot/index.js
@@ -1,20 +1,9 @@
-/*const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-/**/
+/*
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 //fichier ou enrigistre les lvl
-const prefix="m!";
+const prefix="!m";
 
 
 //recupe le token
@@ -68,3 +57,4 @@ return true;
 else
 return false;
 }
+*/
